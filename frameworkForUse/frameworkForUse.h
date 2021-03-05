0711b52d255546b3885dbb82c38dab532e4c8689
Panacea014 <panacea014@panacea014s-imac.local>
2021-03-05 11:58:42 +0530

    Initial Commit

---
+++ frameworkForUse/frameworkForUse.h
@@ -0,0 +1,19 @@
+//
+//  frameworkForUse.h
+//  frameworkForUse
+//
+//  Created by Panacea014 on 05/03/21.
+//  Copyright © 2021 Panacea014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for frameworkForUse.
+FOUNDATION_EXPORT double frameworkForUseVersionNumber;
+
+//! Project version string for frameworkForUse.
+FOUNDATION_EXPORT const unsigned char frameworkForUseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <frameworkForUse/PublicHeader.h>
+
+
